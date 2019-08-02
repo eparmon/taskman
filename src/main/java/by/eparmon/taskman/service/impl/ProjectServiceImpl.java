7d56a7a5fae3ec16c139f440487fc42fa1e9a1f0
eparmon <eparmon@mail.com>
2019-08-02 23:24:04 +0300

    [1.4] added missing Spring annotations

--- src/main/java/by/eparmon/taskman/service/impl/ProjectServiceImpl.java
+++ src/main/java/by/eparmon/taskman/service/impl/ProjectServiceImpl.java
@@ -4,9 +4,11 @@ import by.eparmon.taskman.persistence.model.Project;
 import by.eparmon.taskman.persistence.repository.ProjectRepository;
 import by.eparmon.taskman.service.ProjectService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
+@Service
 @RequiredArgsConstructor
 public class ProjectServiceImpl implements ProjectService {
 
