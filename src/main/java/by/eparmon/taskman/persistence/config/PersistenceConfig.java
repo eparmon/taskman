55ab44f7135e7726b5de1bdc0e7b19d44aff4527
Yauhen <parmon@fortyseven47.com>
2019-08-30 16:24:51 +0300

    [3.2] added profiles

--- src/main/java/by/eparmon/taskman/persistence/config/PersistenceConfig.java
+++ src/main/java/by/eparmon/taskman/persistence/config/PersistenceConfig.java
@@ -1,15 +1,8 @@
 package by.eparmon.taskman.persistence.config;
 
-import by.eparmon.taskman.persistence.repository.ProjectRepository;
-import by.eparmon.taskman.persistence.repository.impl.ProjectRepositoryImpl;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class PersistenceConfig {
 
-    @Bean
-    public ProjectRepository projectRepository() {
-        return new ProjectRepositoryImpl();
-    }
 }
